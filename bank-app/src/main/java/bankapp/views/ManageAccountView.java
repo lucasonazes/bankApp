a33e4d3116d1625a27d932bd54cd8e7db711ebf0
lucasonazes <lucasonazes@gmail.com>
2024-06-21 18:54:21 -0300

    permissions implemented

--- bank-app/src/main/java/bankapp/views/ManageAccountView.java
+++ bank-app/src/main/java/bankapp/views/ManageAccountView.java
@@ -1,6 +1,10 @@
 package bankapp.views;
 
 import javax.swing.*;
+
+import bankapp.models.Account;
+import bankapp.models.Session;
+
 import java.awt.*;
 
 public class ManageAccountView extends JFrame{
@@ -13,6 +17,8 @@ public class ManageAccountView extends JFrame{
     private JButton deletePerson;
     private JButton deleteButton;
     private JButton exitButton;
+    private Session session = Session.getInstance(null, null);
+    private Account account = session.getAccount();
 
     public ManageAccountView() {
         setTitle("Santander");
@@ -27,28 +33,41 @@ public class ManageAccountView extends JFrame{
         gbc.insets = new Insets(10, 10, 10, 10);
         gbc.anchor = GridBagConstraints.CENTER;
 
+        String text = "Bem vindo, " + account.getOwnerName();
+
         gbc.gridx = 0;
         gbc.gridy = 0;
+        JLabel jlabel = new JLabel(text);
+        jlabel.setFont(new Font("System", Font.PLAIN, 24));
+        panel.add(jlabel, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
         createButton = new JButton("CRIAR CONTA");
+        if (account.getOwnerRole().equals("gerente") || account.getOwnerRole().equals("admin")) 
         panel.add(createButton, gbc);
 
-        gbc.gridy = 1;
+        gbc.gridy = 2;
         manageButton = new JButton("GERENCIAR CONTA");
+        if (account.getOwnerRole().equals("cliente"))
         panel.add(manageButton, gbc);
 
-        gbc.gridy = 2;
+        gbc.gridy = 3;
         createPerson = new JButton("CADASTRAR USUÁRIO");
+        if (account.getOwnerRole().equals("gerente") || account.getOwnerRole().equals("admin"))
         panel.add(createPerson, gbc);
 
-        gbc.gridy = 3;
+        gbc.gridy = 4;
         deletePerson = new JButton("EXCLUIR USUÁRIO");
+        if (account.getOwnerRole().equals("gerente") || account.getOwnerRole().equals("admin"))
         panel.add(deletePerson, gbc);
 
-        gbc.gridy = 4;
+        gbc.gridy = 5;
         deleteButton = new JButton("EXCLUIR CONTA");
+        if (account.getOwnerRole().equals("gerente") || account.getOwnerRole().equals("admin"))
         panel.add(deleteButton, gbc);
 
-        gbc.gridy = 5;
+        gbc.gridy = 6;
         exitButton = new JButton("SAIR");
         panel.add(exitButton, gbc);
 
